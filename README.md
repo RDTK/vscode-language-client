1c20c6476610e9fec8734bab5a3946447efa35df
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2019-12-05 15:31:43 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# RDTKLS
+
+RDTK Recipes Syntax Highlight and Language Client for vscode
+
+
+## Extension Settings
+
+* `rdtkls.generator-path` - Path to RDTK build-generator.
---
+++ src/extension.ts
@@ -0,0 +1,62 @@
+
+import * as vscode from 'vscode';
+import { workspace } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export function activate() {
+	console.log('Starting RDTKLS ...');
+
+	let serverBinary = "sh"
+	const bin = vscode.workspace.getConfiguration().get("rdtkls.generator-path")
+	
+	let serverArguments = [ '-c', `LANG=C.UTF-8 BUILD_GENERATOR_CONFIG_FILES= ${bin} language-server` ];
+
+	let serverOptions: ServerOptions = {
+		command: serverBinary,
+		args: serverArguments,
+		transport: TransportKind.socket
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for rdtk recipes
+		documentSelector: [
+			{ language: 'project-recipe' },
+			{ language: 'distribution-recipe' },
+			{ language: 'template-recipe' }
+
+		],
+		synchronize: {
+			// Notify the server about file changes to '.clientrc files contained in the workspace
+			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
+		}
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'rdtkls',
+		'RDTK LS',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+
+	console.log('Congratulations, your extension "RDTKLS" is now active!');
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
